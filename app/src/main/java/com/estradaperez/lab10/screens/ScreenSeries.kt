230304c21fee6f01e5510982c3050b2c8719ec84
Alexander EP <palex2143@gmail.com>
2025-05-27 20:32:25 -0500

    feat: add ContenidoSerieEditar in ScreenSeries.kt for editing series

--- app/src/main/java/com/estradaperez/lab10/screens/ScreenSeries.kt
+++ app/src/main/java/com/estradaperez/lab10/screens/ScreenSeries.kt
@@ -2,20 +2,27 @@ package com.estradaperez.lab10.screens
 
 
 import android.util.Log
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,6 +32,7 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.estradaperez.lab10.model.SerieModel
 import com.estradaperez.lab10.service.SerieApiService
+import kotlinx.coroutines.delay
 
 
 @Composable
@@ -93,3 +101,70 @@ fun ContenidoSeriesListado(navController: NavHostController, servicio: SerieApiS
         }
     }
 }
+
+@Composable
+fun ContenidoSerieEditar(navController: NavHostController, servicio: SerieApiService, pid: Int = 0 ) {
+    var id by remember { mutableStateOf<Int>(pid) }
+    var name by remember { mutableStateOf<String?>("") }
+    var release_date by remember { mutableStateOf<String?>("") }
+    var rating by remember { mutableStateOf<String?>("") }
+    var category by remember { mutableStateOf<String?>("") }
+    var grabar by remember { mutableStateOf(false) }
+
+    if (id != 0) {
+        LaunchedEffect(Unit) {
+            val objSerie = servicio.selectSerie(id.toString())
+            delay(100)
+            name = objSerie.body()?.name
+            release_date = objSerie.body()?.release_date
+            rating = objSerie.body()?.rating.toString()
+            category = objSerie.body()?.category
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        // Spacer(Modifier.height(50.dp))
+        TextField(
+            value = id.toString(),
+            onValueChange = { },
+            label = { Text("ID (solo lectura)") },
+            readOnly = true,
+            singleLine = true
+        )
+        TextField(
+            value = name!!,
+            onValueChange = { name = it },
+            label = { Text("Name: ") },
+            singleLine = true
+        )
+        TextField(
+            value = release_date!!,
+            onValueChange = { release_date = it },
+            label = { Text("Release Date:") },
+            singleLine = true
+        )
+        TextField(
+            value = rating!!,
+            onValueChange = { rating = it },
+            label = { Text("Rating:") },
+            singleLine = true
+        )
+        TextField(
+            value = category!!,
+            onValueChange = { category = it },
+            label = { Text("Category:") },
+            singleLine = true
+        )
+        Button(
+            onClick = {
+                grabar = true
+            }
+        ) {
+            Text("Grabar", fontSize=16.sp)
+        }
+    }
+    }
